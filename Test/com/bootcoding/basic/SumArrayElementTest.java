6cdce87f810df03e99f7f1c7654f376999093124
sakshitmeshram <sakshitmeshram2001@gmai.com>
2022-09-08 13:55:09 +0530

    JUnit Program

---
+++ Test/com/bootcoding/basic/SumArrayElementTest.java
@@ -0,0 +1,26 @@
+package com.bootcoding.basic;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class SumArrayElementTest {
+
+    @Test
+    void test() {
+        SumArrayElement t = new SumArrayElement();
+        int[] a = {1, 4, 1, 7, 9};
+        int[] actualSum = t.getRunningSum(a);
+        int[] expectSum = {1, 5, 6, 13, 22};
+        Assertions.assertArrayEquals(expectSum, actualSum);
+
+    }
+
+    @Test
+    void test1() {
+        SumArrayElement t=new SumArrayElement();
+        int[] a ={1,9,15,70,9};
+        int[] actualSum=t.getRunningSum(a);
+        int [] expectSum={1 ,10 ,25 ,95, 104};
+        Assertions.assertArrayEquals(expectSum, actualSum);
+    }
+}

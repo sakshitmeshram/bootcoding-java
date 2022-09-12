73b375d6540f8e61b3c25d099bab698d48138311
sakshitmeshram <sakshitmeshram2001@gmai.com>
2022-09-12 17:07:56 +0530

    JDBC First Programs

---
+++ src/com/bootcoding/basic/JDBC.java
@@ -0,0 +1,34 @@
+package com.bootcoding.basic;
+
+import java.net.ConnectException;
+import java.sql.*;
+import java.util.Scanner;
+
+public class JDBC {
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter name");
+        String name = sc.nextLine();
+        int id = sc.nextInt();
+
+
+        Class.forName("com.mysql.cj.jdbc.Driver");  //Load and register the driver
+
+
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/sakshit","root","root"); // establish the connection
+
+        PreparedStatement stmt =  con.prepareStatement("insert into student values ?, ?");  // makes a statements
+
+        stmt.setInt(1,id);
+        stmt.setString(2,name);
+
+        stmt.execute();
+
+
+
+
+
+    }
+}

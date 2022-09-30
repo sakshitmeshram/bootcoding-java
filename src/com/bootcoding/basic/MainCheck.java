e4f25862d76c9444fc90e47f6f6de4c17b80ae3b
sakshitmeshram <sakshitmeshram2001@gmai.com>
2022-09-30 14:24:10 +0530

    Run Without Main

---
+++ src/com/bootcoding/basic/MainCheck.java
@@ -0,0 +1,11 @@
+package com.bootcoding.basic;
+
+public class MainCheck {
+    public static void main(int a) {
+        System.out.println(a);
+    }
+
+    public static void main(String[] args) {
+        main(56);
+    }
+}
